de53e94157a0330bfea29a76e7784f9643f82122
WilsonMariano <mgw009@gmail.com>
2018-07-25 16:25:22 -0300

    Vista principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule
